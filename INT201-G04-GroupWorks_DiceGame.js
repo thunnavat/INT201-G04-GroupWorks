51c5d5e6943b298c92a711f8104eda2ed2bad617
Owannnn <73163719+Owannnn@users.noreply.github.com>
2021-09-03 20:52:09 +0700

    Update INT201-G04-GroupWorks_DiceGame.js

--- INT201-G04-GroupWorks_DiceGame.js
+++ INT201-G04-GroupWorks_DiceGame.js
@@ -1,11 +1,9 @@
-
-let dice = {
-    side: 6 //กำหนดค่าลูกเต๋า
-}
+let dice =  6 //กำหนดค่าลูกเต๋า
 function play(round) {// round กำหนดจำนวนรอบ
     for(let count=1;count <= round ;count++){ // กำหนดจำนวนรอบในการเล่น
         player1.roll = rollDice(); //player1 ทอยลูกเต๋า
         player2.roll = rollDice(); //player2 ทอยลูกเต๋า
+		console.log( player1.roll,player2.roll)
         console.log("round :"+count);
         if(player1.roll > player2.roll){ //player1 ชนะ
             player1.result = results.WIN
@@ -13,7 +11,7 @@ function play(round) {// round กำหนดจำนวนรอบ
             console.log(player1);
             console.log(player2);
             console.log(player1.name +":  Win!!")
-        }else if(player1.roll =player2.roll){ // เสมอ
+        }else if(player1.roll === player2.roll){ // เสมอ
             player1.result = results.DRAW
             player2.result = results.DRAW
             console.log(player1);
@@ -37,18 +35,18 @@ const results = {   //เก็บค่าผลการแข่งขัน
 
 
 function rollDice() {   //สุ่มเลขลูกเต๋า
-    return p = Math.floor( 1 + Math.random() * 6 ); //Math.floor: กำหนดผลลัพธ์ให้เป็นจำนวนเต็ม
+    return  Math.floor( 1 + Math.random() * 6 ); //Math.floor: กำหนดผลลัพธ์ให้เป็นจำนวนเต็ม
 }                                                   //Math.random: สุ่มเลขในช่วง 1-6
 
 
 let player1 = { //เก็บค่าต่างๆของ player1
     name: "Mild", //ชื่อ player1
-    roll: dice, //แต้มลูกเต๋า
+    roll: 0, //แต้มลูกเต๋า
     result: results //ผลลัพธ์ที่ได้
 }
 let player2 = { //เก็บค่าต่างๆของ player2
     name: "Mint", //ชื่อ player1
-    roll: dice, //แต้มลูกเต๋า
+    roll: 0, //แต้มลูกเต๋า
     result: results //ผลลัพธ์ที่ได้
 }
 // console.log(player1.name)
