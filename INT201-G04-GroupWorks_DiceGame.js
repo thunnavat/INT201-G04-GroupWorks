be2f86e52079f9c393944ce081917fa56864708a
Sirapope.c <68818447+Sirapope@users.noreply.github.com>
2021-09-05 14:49:22 +0700

    Update INT201-G04-GroupWorks_DiceGame.js

--- INT201-G04-GroupWorks_DiceGame.js
+++ INT201-G04-GroupWorks_DiceGame.js
@@ -1,5 +1,7 @@
 let dice = {side:6} //กำหนดค่าลูกเต๋า
+let playerResult = [];
 function play(round) {// round กำหนดจำนวนรอบ
+    let index = 0; //กำหนดให้ตัวแปร index ให้มีค่าเท่ากับ 0 เพื่อที่จะไว้ใช้กำหนดตำแหน่งเพื่อเก็บข้อมูลใส่ใน array
     for(let count=1;count <= round ;count++){ // กำหนดจำนวนรอบในการเล่น
         player1.roll = rollDice(); //player1 ทอยลูกเต๋า , เข้าถึง key "roll" ผ่าน obj player1 เพื่อเปลี่ยนให้ value ให้เป็นเลขของลูกเต๋าที่ทอยได้
         player2.roll = rollDice(); //player2 ทอยลูกเต๋า , เข้าถึง key "roll" ผ่าน obj player2 เพื่อเปลี่ยนให้ value ให้เป็นเลขของลูกเต๋าที่ทอยได้
@@ -8,7 +10,7 @@ function play(round) {// round กำหนดจำนวนรอบ
             player1.result = results.WIN //เข้าถึง key "result" ผ่าน obj player1 เพื่อเปลี่ยนให้ value ให้เป็น WIN
             player2.result = results.LOSE //เข้าถึง key "result" ผ่าน obj player2 เพื่อเปลี่ยนให้ value ให้เป็น LOSE
             console.log(player1);
-            console.log(player2);
+            console.log(player2); 
             console.log(player1.name +":  Win!!")
         }else if(player1.roll === player2.roll){ // เสมอ
             player1.result = results.DRAW //เข้าถึง key "result" ผ่าน obj player1 เพื่อเปลี่ยนให้ value ให้เป็น DRAW
@@ -24,8 +26,15 @@ function play(round) {// round กำหนดจำนวนรอบ
             console.log(player2.name +":  Win!!")
         }
         
+        for (let key in player1) {
+            playerResult[index++] = player1[key]; // ใช้ for in เพื่อนำ values ใน object ไปเก็บไว้ใน array เพื่อบันทึกข้อมูลการแข่งขันในแต่ละรอบ
+        }
+        for (let key in player2) {
+            playerResult[index++] = player2[key]; // ใช้ for in เพื่อนำ values ใน object ไปเก็บไว้ใน array เพื่อบันทึกข้อมูลการแข่งขันในแต่ละรอบ
+        }        
     }
 }
+
 const results = {   //เก็บค่าผลการแข่งขัน
     WIN: "Win",
     DRAW: "Draw",
@@ -50,5 +59,12 @@ let player2 = { //เก็บค่าต่างๆของ player2
 }
 
 console.log("-------------")
-play(3); //ใส่จำนวนรอบ
+play(2); //ใส่จำนวนรอบ
 console.log("-------------")
+play(3);
+console.log("-------------")
+play(2);
+
+for (let i=0; i < playerResult.length; i++) { //เรียกผลลัพธ์ที่ได้บันทึกทั้งหมด
+    console.log(playerResult[i])
+}
